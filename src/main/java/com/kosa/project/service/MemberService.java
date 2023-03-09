e349269ff79fe2c85a4d466c26b9c748df0e3910
LEE <evisahah@gmail.com>
2023-03-09 20:54:54 +0900

    memberService 에러 수정

--- src/main/java/com/kosa/project/service/MemberService.java
+++ src/main/java/com/kosa/project/service/MemberService.java
@@ -5,9 +5,9 @@ import com.kosa.project.domain.MemberVO;
 public interface MemberService {
 	public Integer insert(MemberVO vo);
 
-	public MemberVO find(Long idx);
+	public MemberVO find(int idx);
 
-	public int remove(Long idx);
+	public int remove(int idx);
 
 	public int modify(MemberVO vo);
 }
--- src/main/java/com/kosa/project/service/MemberServiceImpl.java
+++ src/main/java/com/kosa/project/service/MemberServiceImpl.java
@@ -7,31 +7,31 @@ import com.kosa.project.mapper.MemberMapper;
 
 import lombok.AllArgsConstructor;
 
-// @Service
-// @AllArgsConstructor
-// public class MemberServiceImpl implements MemberService {
+@Service
+@AllArgsConstructor
+public class MemberServiceImpl implements MemberService {
 
-// private MemberMapper memberMapper;
+	private MemberMapper memberMapper;
 
-// @Override
-// public Integer insert(MemberVO vo) {
-// Integer result = memberMapper.insert(vo);
-// return result == null ? null : result;
-// }
+	@Override
+	public Integer insert(MemberVO vo) {
+		Integer result = memberMapper.insert(vo);
+		return result == null ? null : result;
+	}
 
-// @Override
-// public MemberVO find() {
-// return memberMapper.find(idx);
-// }
+	@Override
+	public MemberVO find(int idx) {
+		return memberMapper.find(idx);
+	}
 
-// @Override
-// public int remove(Long idx) {
-// return memberMapper.delete(idx);
-// }
+	@Override
+	public int remove(int idx) {
+		return memberMapper.delete(idx);
+	}
 
-// @Override
-// public int modify(MemberVO vo) {
-// return memberMapper.update(vo);
-// }
+	@Override
+	public int modify(MemberVO vo) {
+		return memberMapper.update(vo);
+	}
 
-// }
+}
