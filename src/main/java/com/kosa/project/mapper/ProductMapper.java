42050857df3f01d503a2687d9873cede2b5af79b
dhrudgns <dhrudgns529@naver.com>
2023-03-09 10:48:16 +0900

    productList

---
+++ src/main/java/com/kosa/project/mapper/ProductMapper.java
@@ -0,0 +1,9 @@
+package com.kosa.project.mapper;
+
+import java.util.List;
+
+import com.kosa.project.domain.ProductVO;
+
+public interface ProductMapper {
+	public List<ProductVO> getProductList(); 
+}
---
+++ src/main/java/com/kosa/project/service/ProductService.java
@@ -0,0 +1,9 @@
+package com.kosa.project.service;
+
+import java.util.List;
+
+import com.kosa.project.domain.ProductVO;
+
+public interface ProductService {
+	public List<ProductVO> getProductList(); 
+}
